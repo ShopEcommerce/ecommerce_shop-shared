dbc648ff11d6007097a8db2a5010dbe81e471590
Truong Quoc Thong <truongquocthong07.14@gmail.com>
2026-05-09 17:23:27 +0700

    feat: add more subjects and queue groups

--- src/events/queue-group-names.ts
+++ src/events/queue-group-names.ts
@@ -1,5 +1,10 @@
 export enum QueueGroupNames {
   AccountService = 'account-service',
   CatalogService = 'catalog-service', 
+  OrderService = 'order-service',
+  ReviewService = 'review-service',
+  PromotionService = 'promotion-service',
+  PaymentService = 'payment-service',
+  ReportService = 'report-service',
   NotificationService = 'notification-service',
 }
